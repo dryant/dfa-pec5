b5fbea8565cbd1fa692944546c500bbd8875f3f8
dryant <dryant@MacBook-Pro-de-dryant.local>
2024-01-11 22:57:13 +0100

    test

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CitylistComponent } from './components/citylist/citylist.component';
-import { MatCardModule } from '@angular/material/card';
+import { CitytableComponent } from './components/citytable/citytable.component';
 
 @NgModule({
   declarations: [AppComponent],
@@ -14,7 +17,10 @@ import { MatCardModule } from '@angular/material/card';
     AppRoutingModule,
     BrowserAnimationsModule,
     MatCardModule,
+    MatMenuModule,
+    MatIconModule,
     CitylistComponent,
+    CitytableComponent,
   ],
   providers: [],
   bootstrap: [AppComponent],
