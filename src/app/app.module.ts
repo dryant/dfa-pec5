1ce81aeb231b4e14849df599c42febab37dc6c75
dryant <dryant@MacBook-Pro-de-dryant.local>
2024-01-11 23:13:34 +0100

    add: add container to html

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CitylistComponent } from './components/citylist/citylist.component';
 import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
   declarations: [AppComponent],
@@ -15,6 +16,7 @@ import { MatCardModule } from '@angular/material/card';
     BrowserAnimationsModule,
     MatCardModule,
     CitylistComponent,
+    MatIconModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
